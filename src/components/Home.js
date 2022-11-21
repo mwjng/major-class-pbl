eeea06cba5268d8974bac0dbc5cbc0e771bbed06
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-21 17:29:48 +0900

    css
    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,42 @@ import interactionPlugin from "@fullcalendar/interaction"
 import productData from "../data/product-data.json";
 import {firestore} from "../firebase_config"
 import firebase from "../firebase_config"
+import styled from "@emotion/styled";
+
+export const StyleWrapper = styled.div`
+
+.fc-day-sun a {
+  color: #FFD700;
+  text-decoration: none;
+}
+
+.fc-day-sat a {
+  color: #FFA500;
+  text-decoration: none;
+}
+.fc td {
+    background: #FFFAF0;
+    color: #6F606E;
+  }
+.fc event{
+  background: #FFFAF0;
+}
+`
 
 class Home extends Component {
     render() {
         return (
-          <div className="App">
+          <div className="App" style={{backgroundColor:"#FFFAF0"}}>
+            <StyleWrapper>
             <FullCalendar 
               defaultView="dayGridMonth" 
               plugins={[ dayGridPlugin, interactionPlugin ]}
               dateClick={this.handleDateClick}
-              eventClick={this.handleEventClick}
+              eventClick={this.handleEventClick}   
               events={productData}
               locale='ko'
             />
+            </StyleWrapper>
           </div>
         );
     }
