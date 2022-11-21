23fa9e45de566ec0cc1384913d417857819b0b9a
kdj <64907633+childevnote@users.noreply.github.com>
2022-11-22 00:28:21 +0900

    kk
    kk

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -3,13 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
+<<<<<<< Updated upstream
 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+=======
+import {getFirestore} from "@firebase/firestore"
+>>>>>>> Stashed changes
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAwUAyreUB_KuEZW3JYRWB1PSIVyjXDNOw",
   authDomain: "calendar-2fb58.firebaseapp.com",
@@ -21,5 +23,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export default firebase.initializeApp(firebaseConfig);
+<<<<<<< Updated upstream
 export const firestore = firebase.firestore;
+=======
+export const firestore = firebase.firestore();
+export const db = getFirestore(app);
+>>>>>>> Stashed changes
