951a269acc372528623ffb964184816e72626ebe
kdj <64907633+childevnote@users.noreply.github.com>
2022-11-22 00:28:21 +0900

    kk
    kk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import Calendar from "./components/Calendar" //캘린더 
-import Error from "./components/Error" //에러
+import { Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => (
   <Routes>
-    <Route path='/' element={<Calendar />}/>
-    <Route path='*' element={<Error />}/>
+    <Route path='/' element={<Home />} />
   </Routes>
 );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { useState, useEffect} from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import productData from "../data/product-data.json";
+<<<<<<< Updated upstream
 import {firestore} from "../firebase_config";
+=======
+import { collection, getDocs } from "firebase/firestore";
+import {firestore} from "../firebase_config"
+>>>>>>> Stashed changes
 import styled from "@emotion/styled";
 
 export const StyleWrapper = styled.div`
@@ -26,6 +32,40 @@ export const StyleWrapper = styled.div`
 }
 `
 
+<<<<<<< Updated upstream
+=======
+const calendar_data = firestore.collection("calendar_data");
+/*
+const [eventsData, setEventsData] = useState({})
+const getEventsData = () => { 
+  firebase.firestore().collection("calendat_data").get().then((snapshot) => {
+    const events = snapshot.docs.map(event => event.data());
+    setEventsData(events)
+    console.log(events)
+  }).catch((e) => {
+    console.log(e + "fetching error")
+  })
+}
+useEffect(() => {
+   getEventsData()
+ },[])
+*/
+
+const [events, setEvents] = useState([]);
+const eventsCollectionRef = collection(db, "calendar_data");
+
+useEffect(()=>{
+  // 비동기로 데이터 받을준비
+  const getEvents = async () => {
+   // getDocs로 컬렉션안에 데이터 가져오기
+    const data = await getDocs(eventsCollectionRef);
+    // users에 data안의 자료 추가. 객체에 id 덮어씌우는거
+    setEvents(data.docs.map((doc)=>({ ...doc.data()})))
+  }
+  getEvents();
+},[])
+
+>>>>>>> Stashed changes
 class Home extends Component {
     render() {
         return (
@@ -35,8 +75,17 @@ class Home extends Component {
               defaultView="dayGridMonth" 
               plugins={[ dayGridPlugin, interactionPlugin ]}
               dateClick={this.handleDateClick}
+<<<<<<< Updated upstream
               eventClick={this.handleEventClick}   
               events={productData}
+=======
+              eventClick={this.handleEventClick}
+              //events={productData}
+              events={events}
+              editable={true}
+              droppable={true}
+              selectable={true}
+>>>>>>> Stashed changes
               locale='ko'
             />
             </StyleWrapper>
@@ -45,6 +94,7 @@ class Home extends Component {
     }
    
     handleDateClick = (arg) => {
+<<<<<<< Updated upstream
       const calendar_data = firestore.collection("calendar_data");
       var a = prompt(arg.dateStr);
       calendar_data.add( { date : arg.dateStr , title : `${a}`})
@@ -52,6 +102,19 @@ class Home extends Component {
 
     handleEventClick = (info) => {
       console.log("")
+=======
+      var plan = prompt(arg.dateStr);
+      if (plan) {
+        calendar_data.doc(arg.dateStr).set( { date : arg.dateStr , title : `${plan}`})
+      }
+    }
+
+    handleEventClick = (info) => {
+      var del = confirm('삭제하시겠습니까?');
+      if (del) {
+        calendar_data.doc(info.event.startStr).delete();
+      }
+>>>>>>> Stashed changes
     }
 }
 export default Home;
