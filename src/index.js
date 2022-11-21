c3c89f242edb2c09b29bb59aea1f670759fbe192
kdj <64907633+childevnote@users.noreply.github.com>
2022-11-22 01:22:42 +0900

    kdj
     아ㅏ아아ㅏ아ㅏ가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import firebase from './firebase_config';
-
 
 const element = document.getElementById('root')
 const root = createRoot(element);
-console.log(firebase);
 root.render(
   <BrowserRouter>
     <App />
