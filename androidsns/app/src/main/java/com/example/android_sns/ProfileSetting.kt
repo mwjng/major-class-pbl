84b3720712791dbf69c383e4ce5d86148db0868b
jiyoonK <wldbs068@gmail.com>
2022-11-22 05:37:08 +0900

    profileImage set profile Nickname set profile message set

--- androidsns/app/src/main/java/com/example/android_sns/ProfileSetting.kt
+++ androidsns/app/src/main/java/com/example/android_sns/ProfileSetting.kt
@@ -2,14 +2,44 @@ package com.example.android_sns
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivityProfilesettingBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ProfileSetting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityProfilesettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // storage rule은 인증이 된 경우에만 접근 가능하도록 되어있음
+        Firebase.auth.currentUser ?: finish()
+
+        val db: FirebaseFirestore = Firebase.firestore
+        //val itemsCollectionRef = db.collection("users")
+
+        binding.confirm.setOnClickListener {
+            // 프로필 수정 작업
+            val nickName = binding.nickname.text
+            val message = binding.message.text
+            val email = Firebase.auth.currentUser?.email.toString()
+
+            val itemMap = hashMapOf(
+                "nickname" to nickName.toString(),
+                "message" to message.toString()
+            )
+            db.collection("users").document(email.toString())
+                .set(itemMap)
+                .addOnSuccessListener {  }
+                .addOnFailureListener {  }
+
+            finish()
+        }
 
         binding.cancel.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
