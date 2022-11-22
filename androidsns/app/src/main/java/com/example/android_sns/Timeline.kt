32939df7554084a5a1ba0652f490028e02a73095
mwjng <wmw04087@gmail.com>
2022-11-22 21:27:12 +0900

    타임라인도 db 연동필요

--- androidsns/app/src/main/java/com/example/android_sns/Timeline.kt
+++ androidsns/app/src/main/java/com/example/android_sns/Timeline.kt
@@ -1,10 +1,12 @@
 package com.example.android_sns
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,12 +34,24 @@ class Timeline : Fragment() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false)
+        val view = inflater.inflate(R.layout.fragment_timeline, container, false)
+
+        var UserList = arrayListOf<User>(
+            User("title","name","hello","1", "1-0"),
+            User("title","name","hello","1", "1-0"),
+            User("title","name","hello","1", "1-0"),
+            User("title","name","hello","1", "1-0"),
+            User("title","name","hello","1", "1-0"))
+
+        var Adapter = ListAdapter(context, UserList)
+        view.findViewById<ListView>(R.id.listview).adapter = Adapter
+
+        return view
     }
 
     companion object {
