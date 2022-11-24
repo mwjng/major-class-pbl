e6b0c69e61118a6166e992bae02d1a38256a12a7
daniel11251 <daniel011125@gmail.com>
2022-11-24 16:15:04 +0900

    데이터 뿌려주기
    ㅇㅇ

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,12 @@
 import FullCalendar from '@fullcalendar/react';
-
 import dayGridPlugin from '@fullcalendar/daygrid';
 import React, { useState } from 'react';
-
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Component } from 'react';
 import interactionPlugin from "@fullcalendar/interaction";
-import productData from "../data/product-data.json";
-import {firestore} from "../firebase_config"
+import { firestore } from "../firebase_config"
+import { getDocs } from "firebase/firestore"
 import styled from "@emotion/styled";
 export const StyleWrapper = styled.div`
 
@@ -29,86 +28,91 @@ export const StyleWrapper = styled.div`
 }
 `
 
-export const Main=()=>{ //시작페이지
-  return(
+export const Main = () => { //시작페이지
+  return (
     <div>
       <h3>TEAM : 2 0 1 8</h3>
-    <h2>C A L E N D A R</h2>
+      <h2>C A L E N D A R</h2>
     </div>
   );
 }
 
 
-export const Error=()=>{ //에러페이지
-    const locations = useLocation();
-    return(
-      <div>
-        <h3>Page not found at {locations.pathname}</h3>
-      </div>
-    );
-  }
-
-
-
-
-
-const calendar_data = firestore.collection("calendar_data");
-/*
-const [eventsData, setEventsData] = useState({})
-const getEventsData = () => { 
-  firebase.firestore().collection("calendat_data").get().then((snapshot) => {
-    const events = snapshot.docs.map(event => event.data());
-    setEventsData(events)
-    console.log(events)
-  }).catch((e) => {
-    console.log(e + "fetching error")
-  })
+export const Error = () => { //에러페이지
+  const locations = useLocation();
+  return (
+    <div>
+      <h3>Page not found at {locations.pathname}</h3>
+    </div>
+  );
 }
-useEffect(() => {
-   getEventsData()
- },[])
-*/
+
+
 const today = new Date();
-const Calendar = (props) => {
+const calendar_data = firestore.collection("calendar_data");
+
+const Calendar = () => {
   const [visible, setVisible] = useState(false);
   const [date, setDate] = useState(today);
+
+  const [eventsData, setEventsData] = useState({})
+  const getEventsData = () => {
+    calendar_data.get().then((snapshot) => {
+      const events = snapshot.docs.map(event => event.data());
+      setEventsData(events)
+      console.log(events)
+    }).catch((e) => {
+      console.log(e + "fetching error")
+    })
+  }
+  useEffect(() => {
+    getEventsData()
+  }, [])
+
+
   const handleDateSelect = (newDate) => {
     setDate(newDate);
-    setVisible(false);
+    setVisible(true);
   };
 
   const handleDateClick = (arg) => { // bind with an arrow function
     console.log(arg)
-      const calendar_data = firestore.collection("calendar_data");
-      var event = prompt("일정을 입력하세요.",);
-      calendar_data.doc(arg.dateStr).set( { date : arg.dateStr , title : `${event}`})
+    var event = prompt("일정을 입력하세요.",);;
+    if (event) {
+      calendar_data.doc(arg.dateStr).set({ date: arg.dateStr, title: `${event}` })
+      console.log("데이터가 추가되었습니다.")
+      this.forcerender();
+    }
+    else
+      console.log("데이터가 null입니다. 추가되지 않았습니다.")
   }
 
   // 클릭 시 이벤트 정보 받아옴
   const handleEventClick = (clickInfo) => {
     console.log(clickInfo.event.id) // id 값 나옴    
-    }
+  }
 
   return (
     <StyleWrapper>
       <FullCalendar
-        plugins={[dayGridPlugin,interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
           right: "dayGridMonth,dayGridWeek,dayGridDay",
         }}
-        selectable = {true}
+        selectable={true}
         dateClick={handleDateClick}
         eventClick={handleEventClick}
         select={handleDateSelect}
         editable={true}
         droppable={true}
         weekends={true}
-        events={productData}
-        
+        events={eventsData}
+        locale='ko'
+
       />
-      </StyleWrapper>
+    </StyleWrapper>
   );
 };
 
