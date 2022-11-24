c9c013643192ad95ba47ab11abe7c12616f35c74
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-24 17:05:04 +0900

    Final
    다합친거

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,48 +2,27 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import '../style/style.css'
 import { Component } from 'react';
 import interactionPlugin from "@fullcalendar/interaction";
 import { firestore } from "../firebase_config"
-import { getDocs } from "firebase/firestore"
-import styled from "@emotion/styled";
-//import Modal from "./components/Modal.js";
-export const StyleWrapper = styled.div`;
-
-.fc-day-sun a {
-  color: #FFD700;
-  text-decoration: none;
-}
-
-.fc-day-sat a {
-  color: #FFA500;
-  text-decoration: none;
-}
-.fc td {
-    background: #FFFAF0;
-    color: #6F606E;
-  }
-.fc event{
-  background: #FFFAF0;
-}
-`
 
 export const Main = () => { //시작페이지
-  return (
-    <div>
-      <h3>TEAM : 2 0 1 8</h3>
-      <h2>C A L E N D A R</h2>
-    </div>
-  );
-}
+    return(
+      <div className='mainPage'>
+        <h1>TEAM : 2 0 1 8</h1>
+      <h1><Link to ="calendar">C A L E N D A R</Link></h1>
+      </div>
+    );
+  }
 
 
 export const Error = () => { //에러페이지
   const locations = useLocation();
   return (
     <div>
-      <h3>Page not found at {locations.pathname}</h3>
+      <h3> not found at {locations.pathname}</h3>
     </div>
   );
 }
@@ -95,7 +74,6 @@ const Calendar = () => {
   }
 
   return (
-    <StyleWrapper>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         headerToolbar={{
@@ -112,9 +90,7 @@ const Calendar = () => {
         weekends={true}
         events={eventsData}
         locale='ko'
-
       />
-    </StyleWrapper>
   );
 };
 
