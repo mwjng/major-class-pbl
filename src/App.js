aa65ecc8e3bd5f640b9367f9708e40795af67d69
daniel11251 <daniel011125@gmail.com>
2022-11-28 16:37:05 +0900

    Insert 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import { Route, Routes } from "react-router-dom";
 import Calendar, {Error, Main} from "./components/Calendar"; //캘린더 
+import Insert from "./components/Insert";
 
 
 const App = () => (
   <Routes>
     <Route path='/' element={<Main />}/>
     <Route path='/calendar' element={<Calendar />}/>
+    <Route path='/calendar/insert' element={<Insert/>}/>
     <Route path='*' element={<Error />}/>
   </Routes>
 );
